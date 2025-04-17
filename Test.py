181c834fdfb27ae1e3dcbdee075e8d37ab5872db
YouTubeMusicAPI <FastMusicYTAPI@gmail.com>
2025-04-18 00:22:37 +0530

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,10 @@
+import asyncio
+ 
+from Yt.Search import Search
+ 
+async def main():
+    results = await Search("Alone Alan Walker")
+    for r in results:
+        print(f"{r.title} -> {r.url}")
+ 
+asyncio.run(main())
